c22530a1a937101581b5877e35a3ef3cd10982e1
Karina Gurachevskaya <k.gurachevskaya@gmail.com>
2020-10-29 23:24:41 +0300

    feat: add News model

--- tut-news/Model/News.swift
+++ tut-news/Model/News.swift
@@ -7,3 +7,25 @@
 //
 
 import Foundation
+
+struct News: Codable {
+    var title: String?
+    var link: String?
+    var description: String?
+    var author: String?
+    var pubDate: Date?
+    var newsUrl: String?
+    
+    
+    init?(with dictionary: [String: Any]?) {
+        guard let dictionary = dictionary else { return }
+        
+        self.title       = dictionary["title"] as? String ?? ""
+        self.link        = dictionary["link"] as? String ?? ""
+        self.description = dictionary["description"] as? String ?? ""
+        self.author      = dictionary["atom:name"] as? String ?? ""
+        self.pubDate     = dictionary["pubDate"] as? Date ?? Date()
+        self.newsUrl     = dictionary["newsUrl"] as? String ?? ""
+        
+    }
+}
